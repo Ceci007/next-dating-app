ecfbb2ceaf74ea0b695cf29710c3b4c86fa4e5cc
UnicornDad8 <cecibenitezca@gmail.com>
2025-09-10 12:45:08 -0300

    Add Navbar component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { AuthProvider } from "@/contexts/auth-context";
+import Navbar from "@/components/Navbar";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -24,12 +25,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="h-full">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased h-full`}
       >
         <AuthProvider>
-          {children}
+          <div className="h-full flex flex-col">
+            <Navbar />
+            {children}
+          </div>
         </AuthProvider>
       </body>
     </html>
